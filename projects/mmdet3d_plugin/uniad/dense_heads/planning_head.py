fix(planning_metrics): fix the bug of wrong dimension of ego car

projects/mmdet3d_plugin/uniad/dense_heads/planning_head.py
@@ -48,8 +48,8 @@ class PlanningHeadSingleMode(nn.Module):
         # Nuscenes
         self.bev_h = bev_h
         self.bev_w = bev_w
-        self.navi_embed = nn.Embedding(3, embed_dims)
-        self.reg_branch = nn.Sequential(
+        self.navi_embed = nn.Embedding(3, embed_dims) # embed the driver command
+        self.reg_branch = nn.Sequential( # map the driver command to planning_steps * 2
             nn.Linear(embed_dims, embed_dims),
             nn.ReLU(),
             nn.Linear(embed_dims, planning_steps * 2),

projects/mmdet3d_plugin/uniad/dense_heads/planning_head_plugin/planning_metrics.py
@@ -47,10 +47,10 @@ class PlanningMetric(Metric):
         segmentation: torch.Tensor (n_future, 200, 200)
         '''
         pts = np.array([ # BUG should be rounded up to integer with larger absolute value
-            [-self.H / 2. + 0.5, self.W / 2.],  # [-1.542, 0.925]
-            [self.H / 2. + 0.5, self.W / 2.],   # [2.542, 0.925]
-            [self.H / 2. + 0.5, -self.W / 2.],  # [2.542, -0.925]
-            [-self.H / 2. + 0.5, -self.W / 2.], # [-1.542, -0.925]
+            [-(self.H / 2. + 0.5), self.W / 2. + 0.5],  # [-2.542, 1.425]
+            [self.H / 2. + 0.5, self.W / 2. + 0.5],   # [2.542, 1.425]
+            [self.H / 2. + 0.5, -(self.W / 2. + 0.5)],  # [2.542, -1.425]
+            [-(self.H / 2. + 0.5), -(self.W / 2. + 0.5)], # [-2.542, -1.425]
         ])
         pts = (pts - self.bx.cpu().numpy()) / (self.dx.cpu().numpy())
         pts[:, [0, 1]] = pts[:, [1, 0]]
@@ -61,7 +61,8 @@ class PlanningMetric(Metric):
         trajs = traj.view(n_future, 1, 2)
         trajs[:,:,[0,1]] = trajs[:,:,[1,0]] # can also change original tensor (y,x)
         trajs = trajs / self.dx
-        trajs = trajs.cpu().numpy() + rc # (n_future, 32, 2) # XXX (4/0.5)*(2/0.5) = 32
+        trajs = trajs.cpu().numpy() + rc # (n_future, 32, 2) # (4/0.5)*(2/0.5) = 32
+        # After bug fix, the number of points p_num should be (5/0.5)*(2/0.5) = 40
         # (n,1,2) + (p_num,2) -> (n,1,2) + (1,p_num,2) -> (n,p_num,2) + (1,p_num,2) -> (n,p_num,2)
 
         r = trajs[:,:,0].astype(np.int32) # (n_future, p_num) p_num = 32 (n_future, 32) y coordinate 